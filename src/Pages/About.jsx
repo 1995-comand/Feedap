6939f7755a241eae8aa43c3b2617005028f3dabe
Ibrohmm <joraboevibrohim5@gmail.com>
2026-01-10 14:20:31 +0500

    asd

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
   <div className="flex justify-center mb-10">
     <img
       src="https://feedup.uz/_next/image?url=https%3A%2F%2Fcdn.zoomda.uz%2Fpages%2F2025%2F06%2F18%2F1750258595660851782.png&w=1080&q=75"
-      alt="Feed Up"
+      alt="Feed Upjon"
       className="rounded-2xl shadow-lg max-w-full h-auto"
     />
   </div>
@@ -20,7 +20,7 @@ const About = () => {
   
   <div className="text-gray-700 text-lg leading-8 space-y-5">
     <p>
-      <strong>Feed Up</strong> — O‘zbekistondagi eng yirik fast-food tarmoqlaridan biri.
+      <strong>Feed Up</strong> — O'zbekistondagi eng yirik fast-food tarmoqlaridan biri.
       Hozirda filiallarimiz soni 50 taga yetdi va kompaniyada 1000 dan ortiq
       jonkuyar xodim faoliyat yuritmoqda.
     </p>
@@ -32,7 +32,7 @@ const About = () => {
 
     <p>
       2016-yilda katta rebrending amalga oshirildi — yangi strategiya,
-      yangi maqsadlar va yangi nom taqdim etildi. Shundan so‘ng brend
+      yangi maqsadlar va yangi nom taqdim etildi. Shundan so'ng brend
       <strong> “Feed Up”</strong> nomi bilan tanildi.
     </p>
 
@@ -40,7 +40,7 @@ const About = () => {
 
     <ul className="list-disc pl-6 space-y-2">
       <li>
-        <strong>Haqiqiy ta’m</strong> — burgerlar, donarlar, lavashlar,
+        <strong>Haqiqiy ta'm</strong> — burgerlar, donarlar, lavashlar,
         sendvichlar va hot-doglar
       </li>
       <li>
@@ -48,7 +48,7 @@ const About = () => {
         yetkazib beruvchilardan olinadi
       </li>
       <li>
-        <strong>Xalqqa yaqinlik</strong> — butun respublika bo‘ylab filiallar
+        <strong>Xalqqa yaqinlik</strong> — butun respublika bo'ylab filiallar
       </li>
       <li>
         <strong>Tezkor yetkazish</strong> — buyurtmalar issiq holda yetkaziladi
@@ -56,9 +56,10 @@ const About = () => {
     </ul>
 
     <p className="font-semibold text-black">
-      Feed Up — bu oddiy fast-food emas. Bu haqiqiy o‘zbekona ta’mga ega
+      Feed Up — bu oddiy fast-food emas. Bu haqiqiy o'zbekona ta,mga ega
       milliy brend!
     </p>
+   
   </div>
 </div>
     
